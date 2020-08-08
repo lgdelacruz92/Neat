1eb506fe599ef8cc450a8f4c720713a114954655
Dela Cruz <ldelacruz@ZHOBM-FZQ0UJL414.attlocal.net>
2020-08-07 18:54:45 -0700

    Graphing neat

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 let neat;
+let pp;
 
 const HEIGHT = 400;
 const NODE_RADIUS = 20;
@@ -6,14 +7,17 @@ const WIDTH = 600;
 
 function setup() {
     createCanvas(WIDTH, HEIGHT);
-    neat = new Neat(2, 1);
+    neat = new Neat(4, 1);
+    neat.mutate(true);
+    neat.mutate(true);
+    pp = pnprint(neat, WIDTH, HEIGHT);
 }
 
 function draw() {
     background(0);
-    let pp = pnprint(neat, WIDTH, HEIGHT);
     printInputLayer(pp);
     printOutputLayer(pp);
+    printHiddenLayers(pp);
     printConnections(pp);
 }
 
@@ -55,5 +59,14 @@ function printConnections(pp) {
         stroke(color);
         strokeWeight(2);
         line(lineLoc[0], lineLoc[1], lineLoc[2], lineLoc[3]);
+    }
+}
+
+function printHiddenLayers(pp) {
+    for (let i = 0; i < pp.hiddenLayers.length; i++) {
+        const nodeLoc = pp.hiddenLayers[i];
+        noStroke();
+        fill(nodeLoc.onColor);
+        circle(nodeLoc.x, nodeLoc.y, NODE_RADIUS);
     }
 }
--- neat_printer.js
+++ neat_printer.js
@@ -10,8 +10,9 @@ const OFF_COLOR = '#a84632';
 function pnprint(neat, width, height) {
     const inputLayer = _getInputLayerLocations(neat, width, height);
     const outputLayer = _getOutputLayerLocations(neat, width, height);
-    const connections = _getConnectionLines(neat, inputLayer, outputLayer);
-    return {inputLayer, outputLayer, connections};
+    const hiddenLayers = _getHiddenLayers(neat, width, height);
+    const connections = _getConnectionLines(neat, inputLayer, outputLayer, hiddenLayers);
+    return {inputLayer, outputLayer, hiddenLayers, connections};
 }
 
 /**
@@ -68,15 +69,58 @@ function _getOutputLayerLocations(neat, width, height) {
  * Gets the line informations of neat connections
  * @param {Neat} neat The neat to get connections for
  */
-function _getConnectionLines(neat, inputLayer, outputLayer) {
+function _getConnectionLines(neat, inputLayer, outputLayer, hiddenLayers) {
     const result = [];
     for (let i = 0; i < neat.connections.length; i++) {
         const connection = neat.connections[i];
         const inNode = connection.inNode;
         const outNode = connection.outNode;
-        const location1 = inputLayer.find(node => node.id === inNode.id);
+        const location0 = inputLayer.find(node => node.id === inNode.id);
+        const location1 = hiddenLayers.find(node => node.id === inNode.id);
+
         const location2 = outputLayer.find(node => node.id === outNode.id);
-        result.push({lineLoc: [location1.x, location1.y, location2.x, location2.y], onColor: ON_COLOR, offColor: OFF_COLOR });
+        const location3 = hiddenLayers.find(node => node.id === outNode.id);
+
+        if (location1 && location3) {
+            result.push({lineLoc: [location1.x, location1.y, location3.x, location3.y], onColor: ON_COLOR, offColor: OFF_COLOR });
+        } else if (location1 && location2) {
+            result.push({lineLoc: [location1.x, location1.y, location2.x, location2.y], onColor: ON_COLOR, offColor: OFF_COLOR });
+        } else if (location0 && location3) {
+            result.push({lineLoc: [location0.x, location0.y, location3.x, location3.y], onColor: ON_COLOR, offColor: OFF_COLOR });
+        } else if (location0 && location2) {
+            result.push({lineLoc: [location0.x, location0.y, location2.x, location2.y], onColor: ON_COLOR, offColor: OFF_COLOR });
+        } else {
+            throw Error('There should never be a situation where whe cant find a node');
+        }
+    }
+    return result;
+}
+
+/**
+ * Function that gets the hidden layer locations
+ * @param {Neat} neat The neat to get connections for
+ * @param {number} width The canvas width
+ * @param {number} height The canvas height
+ *  * @return {Object} Example
+ *                    {
+ *                       x: number,
+ *                       y: number,
+ *                       onColor: #32a852,
+ *                       offColor: #a84632,
+ *                       id: string
+ *                    }
+ */
+function _getHiddenLayers(neat, width, height) {
+    const hiddenLayerStartIndex = neat.inputNumber + neat.outputNumber - 1;
+    const totalNodes = neat.nodes.length;
+    const result = [];
+    const numHiddenLayers = totalNodes - neat.inputNumber - neat.outputNumber;
+    for (let i = 1; i <= numHiddenLayers; i++) {
+        const randomXHiddenLocation = (Math.random() *7)/8 + (1/8);
+        const increments = 1 / (numHiddenLayers + 1);
+        const nodeId = neat.nodes[hiddenLayerStartIndex + i].id;
+
+        result.push({ x: randomXHiddenLocation * width, y: i * increments * height, onColor: ON_COLOR, offColor: OFF_COLOR, id: nodeId })
     }
     return result;
 }
