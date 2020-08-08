98ed86186680b361413ea370f15c7c3f908fcfeb
Dela Cruz <ldelacruz@ZHOBM-FZQ0UJL414.attlocal.net>
2020-08-08 11:31:03 -0700

    Network parents are all set up.

--- neat_crossover_tests.js
+++ neat_crossover_tests.js
@@ -10,4 +10,72 @@ const test_neat_21 = () => {
     assertEqualNoTitle(childNeat.nodes.length, neat1.nodes.length + 1);
 }
 
+const test_neat_22 = () => {
+    const INPUTNUMBER1 = 3;
+    const OUTPUTNUMBER1 = 2;
+    const LETTERS = 'abcdefghijklmnopqrstuvwxyz';
+    const neat1 = new Neat(INPUTNUMBER1, OUTPUTNUMBER1);
+    for (let i = 0; i < INPUTNUMBER1; i++) {
+        neat1.nodes[i].id = `${i}`;
+    }
+
+    for (let i = INPUTNUMBER1; i < INPUTNUMBER1 + OUTPUTNUMBER1; i++) {
+        neat1.nodes[i].id = `${i}`;
+    }
+
+    for (let i = 0; i < neat1.connections.length; i++) {
+        neat1.connections[i].in = `${LETTERS[i]}`;
+    }
+
+    for (let i = 0; i < INPUTNUMBER1 + OUTPUTNUMBER1; i++) {
+        assertEqual(neat1.nodes[i].id === `${i}`, true, 'Neat crossover stress test 1.');
+    }
+
+    for (let i = 0; i < neat1.connections.length; i++) {
+        assertEqualNoTitle(neat1.connections[i].in, `${LETTERS[i]}`);
+    }
+
+    const INPUTNUMBER2 = 3;
+    const OUTPUTNUMBER2 = 2;
+    const neat2 = new Neat(INPUTNUMBER2, OUTPUTNUMBER2);
+    for (let i = 0; i < INPUTNUMBER2; i++) {
+        neat2.nodes[i].id = `${i}`;
+    }
+
+    for (let i = INPUTNUMBER2; i < INPUTNUMBER2 + OUTPUTNUMBER1; i++) {
+        neat2.nodes[i].id = `${i}`;
+    }
+
+    for (let i = 0; i < neat2.connections.length; i++) {
+        neat2.connections[i].in = `${LETTERS[i]}`;
+    }
+
+    const node5 = new Node("5");
+    const connectionG = new Connection('g', 0, true);
+    const connectionH = new Connection('h', 0, true);
+    neat2.connections.splice(1, 1);
+    connectionG.inNode = neat2.nodes[1];
+    connectionG.outNode = node5;
+    connectionH.inNode = node5;
+    connectionH.outNode = neat2.nodes[3];
+    neat2.nodes.push(node5);
+    neat2.connections.push(connectionG);
+    neat2.connections.push(connectionH);
+    for (let i = 0; i < neat2.nodes.length; i++) {
+        assertEqualNoTitle(neat2.nodes[i].id === `${i}`, true);
+    }
+    assertEqualNoTitle(neat2.connections.length, 7);
+
+    for (let i = 0; i < neat2.connections.length + 1; i++) {
+        if (i > 1) {
+            assertEqualNoTitle(neat2.connections[i-1].in, `${LETTERS[i]}`);
+        } else {
+            if (i !== 1) {
+                assertEqualNoTitle(neat2.connections[i].in, `${LETTERS[i]}`);
+            }
+        }
+    }
+}
+
 test_neat_21(); 
+test_neat_22(); 
